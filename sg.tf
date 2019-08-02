bd7cb8afd8859db2f11d5d7294aa739656fbd1dd
luton66 <luton66@gmail.com>
2019-08-02 14:55:04 +0100

    updates to security group for egress

--- sg.tf
+++ sg.tf
@@ -9,11 +9,25 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port = 3389
     to_port = 3389
     protocol = "udp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
   }
 
   tags {
